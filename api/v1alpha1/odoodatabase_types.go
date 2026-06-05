6150218670f420ad9307ef68c42a63a9de73855b
rodrigo.microsiga <rodrigo.microsiga@icloud.com>
2026-06-05 14:16:49 -0300

    feat: modela os CRDs OdooInstance e OdooDatabase
    Detalhes:
    - Define Spec e Status para o servidor e tenant conforme o design oficial.
    - Adiciona markers do kubebuilder para validação automática, valores default e colunas personalizadas no terminal.
    - Gera os manifestos YAML (make manifests) e métodos DeepCopy.

--- api/v1alpha1/odoodatabase_types.go
+++ api/v1alpha1/odoodatabase_types.go
@@ -1,92 +1,61 @@
-/*
-Copyright 2026.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// OdooDatabaseSpec defines the desired state of OdooDatabase
-type OdooDatabaseSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
-
-	// foo is an example field of OdooDatabase. Edit odoodatabase_types.go to remove/update
-	// +optional
-	Foo *string `json:"foo,omitempty"`
+// InstanceReference aponta para o OdooInstance pai
+type InstanceReference struct {
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
 }
 
-// OdooDatabaseStatus defines the observed state of OdooDatabase.
+// OdooDatabaseSpec define o estado desejado do banco lógico
+type OdooDatabaseSpec struct {
+	// +kubebuilder:validation:Required
+	InstanceRef InstanceReference `json:"instanceRef"`
+
+	// DbName é o nome do banco de dados no PostgreSQL
+	// +kubebuilder:validation:Required
+	DbName string `json:"dbName"`
+
+	// Domain é a URL exclusiva deste tenant
+	// +kubebuilder:validation:Required
+	Domain string `json:"domain"`
+
+	// +optional
+	Lang string `json:"lang,omitempty"`
+
+	// +optional
+	DemoData bool `json:"demoData,omitempty"`
+}
+
+// OdooDatabaseStatus define o estado observado da criação do banco
 type OdooDatabaseStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// +optional
+	Phase string `json:"phase,omitempty"`
 
-	// For Kubernetes API conventions, see:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
+	// +optional
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
 
-	// conditions represent the current state of the OdooDatabase resource.
-	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
-	//
-	// Standard condition types include:
-	// - "Available": the resource is fully functional
-	// - "Progressing": the resource is being created or updated
-	// - "Degraded": the resource failed to reach or maintain its desired state
-	//
-	// The status of each condition is one of True, False, or Unknown.
-	// +listType=map
-	// +listMapKey=type
 	// +optional
 	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Instance",type=string,JSONPath=`.spec.instanceRef.name`
+// +kubebuilder:printcolumn:name="DB Name",type=string,JSONPath=`.spec.dbName`
+// +kubebuilder:printcolumn:name="Domain",type=string,JSONPath=`.spec.domain`
+// +kubebuilder:printcolumn:name="Phase",type=string,JSONPath=`.status.phase`
 
 // OdooDatabase is the Schema for the odoodatabases API
 type OdooDatabase struct {
-	metav1.TypeMeta `json:",inline"`
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	// metadata is a standard object metadata
-	// +optional
-	metav1.ObjectMeta `json:"metadata,omitzero"`
-
-	// spec defines the desired state of OdooDatabase
-	// +required
-	Spec OdooDatabaseSpec `json:"spec"`
-
-	// status defines the observed state of OdooDatabase
-	// +optional
-	Status OdooDatabaseStatus `json:"status,omitzero"`
+	Spec   OdooDatabaseSpec   `json:"spec,omitempty"`
+	Status OdooDatabaseStatus `json:"status,omitempty"`
 }
 
-// +kubebuilder:object:root=true
-
-// OdooDatabaseList contains a list of OdooDatabase
-type OdooDatabaseList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitzero"`
-	Items           []OdooDatabase `json:"items"`
-}
-
-func init() {
-	SchemeBuilder.Register(&OdooDatabase{}, &OdooDatabaseList{})
-}
+// ... (Mantenha o restante do arquivo OdooDatabaseList e init sem alterações)
--- api/v1alpha1/odooinstance_types.go
+++ api/v1alpha1/odooinstance_types.go
@@ -1,92 +1,84 @@
-/*
-Copyright 2026.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// OdooInstanceSpec defines the desired state of OdooInstance
-type OdooInstanceSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
-
-	// foo is an example field of OdooInstance. Edit odooinstance_types.go to remove/update
+// OdooAppSpec define os parâmetros do container do Odoo
+type OdooAppSpec struct {
+	// +kubebuilder:default:="odoo:17.0"
 	// +optional
-	Foo *string `json:"foo,omitempty"`
+	Image string `json:"image,omitempty"`
+
+	// +kubebuilder:default:=1
+	// +kubebuilder:validation:Minimum:=0
+	// +optional
+	Replicas int32 `json:"replicas,omitempty"`
+
+	// +kubebuilder:default:="5Gi"
+	// +optional
+	StorageSize resource.Quantity `json:"storageSize,omitempty"`
 }
 
-// OdooInstanceStatus defines the observed state of OdooInstance.
+// DatabaseSpec define os parâmetros do container do PostgreSQL
+type DatabaseSpec struct {
+	// +kubebuilder:default:="postgres:16"
+	// +optional
+	Image string `json:"image,omitempty"`
+
+	// +kubebuilder:default:="odoo"
+	// +optional
+	User string `json:"user,omitempty"`
+
+	// +kubebuilder:default:="5Gi"
+	// +optional
+	StorageSize resource.Quantity `json:"storageSize,omitempty"`
+}
+
+// OdooInstanceSpec define o estado desejado pelo usuário
+type OdooInstanceSpec struct {
+	// Domain é o host base do servidor (obrigatório)
+	// +kubebuilder:validation:Required
+	Domain string `json:"domain"`
+
+	// +optional
+	Odoo OdooAppSpec `json:"odoo,omitempty"`
+
+	// +optional
+	Database DatabaseSpec `json:"database,omitempty"`
+
+	// +optional
+	IngressClassName string `json:"ingressClassName,omitempty"`
+}
+
+// OdooInstanceStatus define o estado real observado pelo Operator
 type OdooInstanceStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// +optional
+	Phase string `json:"phase,omitempty"`
 
-	// For Kubernetes API conventions, see:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
+	// +optional
+	URL string `json:"url,omitempty"`
 
-	// conditions represent the current state of the OdooInstance resource.
-	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
-	//
-	// Standard condition types include:
-	// - "Available": the resource is fully functional
-	// - "Progressing": the resource is being created or updated
-	// - "Degraded": the resource failed to reach or maintain its desired state
-	//
-	// The status of each condition is one of True, False, or Unknown.
-	// +listType=map
-	// +listMapKey=type
+	// +optional
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
+
 	// +optional
 	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Phase",type=string,JSONPath=`.status.phase`
+// +kubebuilder:printcolumn:name="URL",type=string,JSONPath=`.status.url`
 
 // OdooInstance is the Schema for the odooinstances API
 type OdooInstance struct {
-	metav1.TypeMeta `json:",inline"`
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	// metadata is a standard object metadata
-	// +optional
-	metav1.ObjectMeta `json:"metadata,omitzero"`
-
-	// spec defines the desired state of OdooInstance
-	// +required
-	Spec OdooInstanceSpec `json:"spec"`
-
-	// status defines the observed state of OdooInstance
-	// +optional
-	Status OdooInstanceStatus `json:"status,omitzero"`
+	Spec   OdooInstanceSpec   `json:"spec,omitempty"`
+	Status OdooInstanceStatus `json:"status,omitempty"`
 }
 
-// +kubebuilder:object:root=true
-
-// OdooInstanceList contains a list of OdooInstance
-type OdooInstanceList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitzero"`
-	Items           []OdooInstance `json:"items"`
-}
-
-func init() {
-	SchemeBuilder.Register(&OdooInstance{}, &OdooInstanceList{})
-}
+// ... (Mantenha o restante do arquivo OdooInstanceList e init sem alterações)
